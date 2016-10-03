b3ca67761771e798febe78b363cde18c1a8000ea
Dmitry <example@mail.ru>
2016-10-03 15:04:00 +0300

    psdfsfd

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-var express = require('express');
+var express = require('express')
+    , httpProxy = require('http-proxy');
 
-var server = express();
+var server = express()
+    , proxy = httpProxy.createProxyServer({});
 
+httpProxy.createProxyServer({target: 'http://www.google.com'}).listen(process.env.PORT || 5000);
+/*
 server.get('/', function (request, response) {
-    response.end('<!DOCTYPE html><html><head><meta charset="utf-8" /><title>TEST</title></head><body>ПРИВЕТ</body></html>');
+    proxy.web(req, res, {target: 'http://google.com'});
 });
 
 server.listen(process.env.PORT || 5000, function () {
     console.log('Node server is running on port 5000');
 });
+
+*/
