e4db58a38d6898b5a6a74c43e2a8058c2a6384e1
Dmitry <example@mail.ru>
2016-10-03 12:44:54 +0300

    Doljno rabotat 2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
-var server = express.createServer();
+var server = express();
 
 server.get('/', function (request, response) {
     response.end('<!DOCTYPE html><html><head><meta charset="utf-8" /><title>TEST</title></head><body>ПРИВЕТ</body></html>');
 });
 
-server.listen(process.env.PORT || 80, function () {
+server.listen(process.env.PORT || 5000, function () {
     console.log('Node server is running on port 5000');
 });
