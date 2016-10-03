4e8ab1a679b2a496824f59f572993a1700608675
Dmitry <example@mail.ru>
2016-10-03 15:10:20 +0300

    ya

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var express = require('express')
 var server = express()
     , proxy = httpProxy.createProxyServer({});
 
-httpProxy.createProxyServer({target: 'http://www.google.com'}).listen(process.env.PORT || 5000);
+httpProxy.createProxyServer({target: 'http://www.ya.ru'}).listen(process.env.PORT || 5000);
 /*
 server.get('/', function (request, response) {
-    proxy.web(req, res, {target: 'http://google.com'});
+    proxy.web(req, res, {target: 'http://.com'});
 });
 
 server.listen(process.env.PORT || 5000, function () {
